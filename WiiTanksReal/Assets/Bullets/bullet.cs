92318f54c53c349bdce11a9c29b409e96979f0b9
gmamafilms2 <gmamafilms@gmail.com>
2019-05-08 18:45:44 -0700

    bullet changes
    made the bullet disapear after hitting the wall the number of times

--- WiiTanksReal/Assets/Bullets/bullet.cs
+++ WiiTanksReal/Assets/Bullets/bullet.cs
@@ -8,27 +8,51 @@ public class bullet : MonoBehaviour
     Rigidbody rigid;
     Collider collider;
     public float speed = 1.0f;
+    public int maxNumberOfWallHits = 0;
+    private int numOfHits;
 
     // Start is called before the first frame update
     void Start()
     {
+        //gets the rigid body 
         rigid = GetComponent<Rigidbody>();
+        //gets the collider
         collider = GetComponent<Collider>();
-        //rigid.AddForce(transform.forward * speed);
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        //checks to see if we hit the walls too many times
+        if (numOfHits >= maxNumberOfWallHits)
+        {
+            /*
+             * make smoke particles spawn here 
+             */
+
+            //destroy this game object if we have hit the wall
+            Destroy(gameObject);
+        }
+        //this moves the posion of the bullet every frame so that it moves strait and has no curve
+        //or gravity
         transform.position += transform.forward * Time.deltaTime * speed;
+
     }
 
+    //this is called every time the collider hits an object with a collider
     void OnCollisionEnter(Collision col)
     {
+        //checks to see if we hit a wall
         if (col.gameObject.tag == "Wall")
         {
+            //this gets the reflected vector and sets the forward direction of this object to that vector
             transform.forward = Vector3.Reflect(transform.forward, col.GetContact(0).normal);
-        } else if (col.gameObject.tag == "Tank")
+            numOfHits++;
+        } 
+        //checks to see if we hit a tank
+        else if (col.gameObject.tag == "Tank")
         {
 
         }
