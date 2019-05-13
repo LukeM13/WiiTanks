1ffb15bca5fa40fce8ab0079980de4417480ebdb
gmamafilms2 <gmamafilms@gmail.com>
2019-05-13 13:53:36 -0700

    Changed Split
    Made the split not inherit from bulletparent

--- WiiTanksReal/Assets/Bullets/Scripts/bullet.cs
+++ WiiTanksReal/Assets/Bullets/Scripts/bullet.cs
@@ -83,18 +83,5 @@ public class bullet : MonoBehaviour
         }
     }
 
-    //this is a function that gets the interfaces from all the scripts on an object
-    public static void GetInterfaces<T>(out List<T> resultList, GameObject objectToSearch) where T : class
-    {
-        MonoBehaviour[] list = objectToSearch.GetComponents<MonoBehaviour>();
-        resultList = new List<T>();
-        foreach (MonoBehaviour mb in list)
-        {
-            if (mb is T)
-            {
-                //found one
-                resultList.Add((T)((System.Object)mb));
-            }
-        }
-    }
+
 }
