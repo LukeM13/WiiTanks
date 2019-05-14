521adc88644ae8470749a259186ddd62738119e1
Jash Garish <jashgarish@gmail.com>
2019-05-14 13:19:00 -0700

    Nothing much

---
+++ WiiTanksReal/Assets/GlobalFunctions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalFunctions : MonoBehaviour
+{
+
+    //this is a function that gets the interfaces from all the scripts on an object
+    public static void GetInterfaces<T>(out List<T> resultList, GameObject objectToSearch) where T : class
+    {
+        MonoBehaviour[] list = objectToSearch.GetComponents<MonoBehaviour>();
+        resultList = new List<T>();
+        foreach (MonoBehaviour mb in list)
+        {
+            if (mb is T)
+            {
+                //found one
+                resultList.Add((T)((System.Object)mb));
+            }
+        }
+    }
+
+}
