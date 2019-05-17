a68e4d29533a2caae2bb190a674f90359fd7660e
piecewise <gmamafilms@gmail.com>
2019-05-16 21:35:13 -0700

    Big facts.com
    got the level changing done

--- WiiTanksReal/Assets/Scenes/BulletSelect/BulletSelect.cs
+++ WiiTanksReal/Assets/Scenes/BulletSelect/BulletSelect.cs
@@ -1,22 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+using UnityEditor;
 
 public class BulletSelect : MonoBehaviour
 {
-    public Object tankType;
+    public Object tankUI;
 
-    private List<object> tankTypes;
+    private GameMode gameMode;
+
+    public VerticalLayoutGroup tankHolder;
 
     // Start is called before the first frame update
     void Start()
     {
         
+        gameMode = GameObject.FindGameObjectWithTag("GameMode").GetComponent<GameMode>();
+
+        foreach (TankData tank in gameMode.getNextLevelInfo().tankInfo)
+        {
+           
+            //create a new tankUI object for the all the tanks in the level
+            GameObject tankObject = (GameObject)Instantiate(tankUI);
+            //set this transform the the tankholder so that it aligns with everything
+            tankObject.transform.SetParent(tankHolder.transform, false);
+            //change the tank values so that the ui reflects this properly
+            TankUIScript tankValues = tankObject.GetComponent<TankUIScript>();
+            //update these values
+            print("tank type: " + tank.type.ToString() +  ", count: " + tank.number);
+            tankValues.updateValues(tank.type, tank.number);
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void goToNextLevel()
+    {
+        gameMode.nextLevel();
     }
 }
---
+++ WiiTanksReal/Assets/Scenes/BulletSelect/TankUIScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class TankUIScript : MonoBehaviour
+{
+
+    private TankType type;
+
+    private string count;
+
+    public GameObject nameObject;
+    private TMP_Text name;
+    public GameObject tankCountObject;
+    private TMP_Text tankCount;
+
+
+    private void Start()
+    {
+        
+
+    }
+
+    public void updateValues(TankType tt, int num)
+    {
+        name = nameObject.GetComponent<TMP_Text>();
+        tankCount = tankCountObject.GetComponent<TMP_Text>();
+        type = tt;
+        count = "x " + num;
+        name.SetText(tt.ToString());
+        tankCount.SetText(count);
+    }
+}
