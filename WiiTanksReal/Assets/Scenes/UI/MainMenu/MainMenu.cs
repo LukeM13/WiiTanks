7abd9317279448a19f88072457d300d33ddb3d22
gmamafilms2 <gmamafilms@gmail.com>
2019-05-24 13:42:37 -0700

    Main menu

--- WiiTanksReal/Assets/Scenes/UI/MainMenu/MainMenu.cs
+++ WiiTanksReal/Assets/Scenes/UI/MainMenu/MainMenu.cs
@@ -1,11 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
+    public GameObject returnButton;
+    private bool showReturn;
+
+    public GameObject instructionsMenu;
+
+    private GameMode gameMode;
+
     public void PlayGame()
     {
-        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+ 1);
+        gameMode = GameObject.FindGameObjectWithTag("GameMode").GetComponent<GameMode>();
+        DontDestroyOnLoad(GetComponent<AudioSource>());
+        gameMode.loadFirstLevel();
+        Debug.Log("next scene");
+    }
+
+    public void openInstructions()
+    {
+        returnButton.SetActive(true);
+        instructionsMenu.SetActive(true);
+    }
+
+    public void returnToMenu()
+    {
+        returnButton.SetActive(false);
+        instructionsMenu.SetActive(false);
     }
 }
