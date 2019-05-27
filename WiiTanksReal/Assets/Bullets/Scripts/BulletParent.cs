712de2099f2440331c4178980901ab7ea2037e37
piecewise <gmamafilms@gmail.com>
2019-05-26 19:58:11 -0700

    added mission number
    added mission number and fixed some bullets not richoeting

--- WiiTanksReal/Assets/Bullets/Scripts/BulletParent.cs
+++ WiiTanksReal/Assets/Bullets/Scripts/BulletParent.cs
@@ -18,68 +18,62 @@ public class BulletParent : MonoBehaviour
     public ParticleSystem deathParticle;
     protected bool destroy;
 
+
+    protected bool stillInWall = false;
     //this is called every time the collider hits an object with a collider
     protected void OnCollisionEnter(Collision col)
     {
-        //checks to see if we hit a wall
-        if (col.gameObject.tag.Equals("Wall"))
-        {
-            //if we hit a damage able wall we want to damage the wall and destroy the bullet
-
-            //get the damage interface from the scripts on the wall
-            List<Damageable> damageScripts; ;
-            //returns a list of damageable interface
-            GlobalFunctions.GetInterfaces<Damageable>(out damageScripts, col.gameObject);
-            //loop through the interfaces and damage the objects
-            foreach (Damageable damageObject in damageScripts)
+        if (!stillInWall) {
+            //checks to see if we hit a wall
+            if (col.gameObject.tag.Equals("Wall"))
             {
-                //apply the damage
-                damageObject.damage(damage, this.gameObject);
-                //we want to destroy the object after
-                destroy = true;
-                //return so it doesn't reflect
-                return;
+
+                //this gets the reflected vector and sets the forward direction of this object to that vector
+                transform.forward = Vector3.Reflect(transform.forward, col.GetContact(0).normal);
+                numOfHits++;
+                stillInWall = true;
             }
-
-            //this gets the reflected vector and sets the forward direction of this object to that vector
-            transform.forward = Vector3.Reflect(transform.forward, col.GetContact(0).normal);
-
-            numOfHits++;
-        }
-        else if (col.gameObject.tag.Equals("Boundry"))
-        {
-            //this gets the reflected vector and sets the forward direction of this object to that vector
-            transform.forward = Vector3.Reflect(transform.forward, col.GetContact(0).normal);
-            numOfHits++;
-        }
-        //checks to see if we hit a tank
-        else if (col.gameObject.tag.Equals("Tank"))
-        {
-            //same thing as the wall
-            List<Damageable> damageScripts;
-            GlobalFunctions.GetInterfaces<Damageable>(out damageScripts, col.gameObject);
-            foreach (Damageable damageObject in damageScripts)
+            else if (col.gameObject.tag.Equals("Boundry"))
             {
-                destroy = true;
-                damageObject.damage(damage, this.gameObject);
+                //this gets the reflected vector and sets the forward direction of this object to that vector
+                transform.forward = Vector3.Reflect(transform.forward, col.GetContact(0).normal);
+                numOfHits++;
+                stillInWall = true;
             }
+            //checks to see if we hit a tank
+            else if (col.gameObject.tag.Equals("Tank"))
+            {
+                //same thing as the wall
+                List<Damageable> damageScripts;
+                GlobalFunctions.GetInterfaces<Damageable>(out damageScripts, col.gameObject);
+                foreach (Damageable damageObject in damageScripts)
+                {
+                    destroy = true;
+                    damageObject.damage(damage, this.gameObject);
+                }
 
-        }
-        else if (col.gameObject.tag.Equals("Player"))
-        {
-            //same thing as the wall
-            List<Damageable> damageScripts;
-            GlobalFunctions.GetInterfaces<Damageable>(out damageScripts, col.gameObject);
-            foreach (Damageable damageObject in damageScripts)
+            }
+            else if (col.gameObject.tag.Equals("Player"))
+            {
+                //same thing as the wall
+                List<Damageable> damageScripts;
+                GlobalFunctions.GetInterfaces<Damageable>(out damageScripts, col.gameObject);
+                foreach (Damageable damageObject in damageScripts)
+                {
+                    destroy = true;
+                    damageObject.damage(damage, this.gameObject);
+                }
+            }
+            else if (col.gameObject.tag.Equals("Bullet"))
             {
                 destroy = true;
-                damageObject.damage(damage, this.gameObject);
             }
+            
         }
-        else if (col.gameObject.tag.Equals("Bullet"))
-        {
-            destroy = true;
-        }
+    }
 
+    protected void OnCollisionExit(Collision collision)
+    {
+        stillInWall = false;
     }
 }
