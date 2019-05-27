cad1b941ce7d864098622b1a8f91a87fbc90af9f
gmamafilms2 <gmamafilms@gmail.com>
2019-05-27 09:21:07 -0700

    build settings

--- WiiTanksReal/Assets/Scenes/GameMode.cs
+++ WiiTanksReal/Assets/Scenes/GameMode.cs
@@ -1,8 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 using UnityEditor;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using System;
 
